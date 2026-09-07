fa3f71d21dfa05c7a98c66231397b06f15e84e15
zjucc2005 <zjucc2005@163.com>
2026-09-07 14:39:11 +0800

    v2.0.4

---
+++ assets/scan_code.ByFW_fbR.js
@@ -0,0 +1,1 @@
+import"./index-CRkfyPqu.js";const s=()=>new Promise(((s,e)=>e("PC版不支持扫码")));export{s};
--- assets/uni-app.es.Da5TXhqX.js
+++
@@ -1,1 +0,0 @@
-import"./index-Btf08UUs.js";function r(r,t){return"string"==typeof r?t:r}export{r};
