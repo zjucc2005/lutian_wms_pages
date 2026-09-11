5029b133c63792e87d50a180d0cf7b9acbd25c70
zjucc2005 <zjucc2005@163.com>
2026-09-11 15:18:51 +0800

    v2.0.6

---
+++ assets/scan_code.B0KUsHc6.js
@@ -0,0 +1,1 @@
+import"./index-CU9MS-gN.js";const s=()=>new Promise(((s,e)=>e("PC版不支持扫码")));export{s};
--- assets/uni-app.es.Cqhzok_o.js
+++
@@ -1,1 +0,0 @@
-import"./index-BP9VCiN8.js";function r(r,t){return"string"==typeof r?t:r}export{r};
